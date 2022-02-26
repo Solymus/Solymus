f13610d9be1d4d8536f323350a5aa09af4660428
KayllonDouglas <kayllon.doug10@gmail.com>
2022-02-26 14:37:02 -0300

    Updates, Additions, a lot of mess...

---
+++ src/main/java/com/github/solymus/material/info/MaterialInfo.java
@@ -0,0 +1,20 @@
+package com.github.solymus.material.info;
+
+import com.github.solymus.material.MaterialId;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MaterialInfo {
+
+    String name();
+
+    int id();
+
+    int meta() default 0;
+
+}

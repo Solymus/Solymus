a83718153de043e300008518e673411372c19ed8
KKD4yv1d <kkd4yv1d@gmail.com>
2022-02-25 21:30:52 -0300

    Add server logger

--- src/main/java/com/github/solymus/Solymus.java
+++ src/main/java/com/github/solymus/Solymus.java
@@ -1,14 +1,27 @@
 package com.github.solymus;
 
-import com.github.solymus.server.ServerSingleton;
+import com.github.solymus.logger.MainLogger;
+import com.github.solymus.server.Server;
 
 public class Solymus {
 
-    /**
-     * Class responsible for creating a single instance of the Server class.
-     */
-    public static void main(String[] args) {
-        new ServerSingleton();
+  public static boolean ANSI = true;
+  public static String VERSION = "1.0.0";
+  public static boolean STABLE_REALASE = false;
+
+  public static void main(String[] args) {
+
+    if (args.length != 0) {
+      for (String arg : args) {
+        if (arg.equals("--no-ansi") || arg.equals("--disable-ansi"))
+          ANSI = false;
+      }
     }
+
+    MainLogger logger = new MainLogger(ANSI);
+    Server server = new Server(logger);
+
+    // TODO: Add an "Server#start()" here
+  }
 
 }
--- src/main/java/com/github/solymus/utils/Utils.java
+++ src/main/java/com/github/solymus/utils/Utils.java
@@ -1,9 +1,21 @@
 package com.github.solymus.utils;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public final class Utils {
 
   public static boolean isNull(Object object) {
     return object == null;
+  }
+
+  public static String getExceptionMessage(Exception exception) {
+    StringWriter writer = new StringWriter();
+    PrintWriter printer = new PrintWriter(writer);
+
+    exception.printStackTrace(printer);
+    printer.close();
+    return writer.toString();
   }
 
 }

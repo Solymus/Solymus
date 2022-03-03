655ef1205ed6e38262dc083028cecb6a00a29f3a
KayllonDouglas <kayllon.doug10@gmail.com>
2022-03-03 11:24:19 -0300

    Added SkinData

---
+++ src/main/java/com/github/solymus/entity/data/SkinData.java
@@ -0,0 +1,12 @@
+package com.github.solymus.entity.data;
+
+public class SkinData {
+
+    private static final int PIXEL_SIZE = 4;
+
+    public static final int SINGLE_SKIN_SIZE = 64 * 32 * PIXEL_SIZE;
+    public static final int DOUBLE_SKIN_SIZE = 64 * 64 * PIXEL_SIZE;
+    public static final int SKIN_128_64_SIZE = 128 * 64 * PIXEL_SIZE;
+    public static final int SKIN_128_128_SIZE = 128 * 128 * PIXEL_SIZE;
+
+}
